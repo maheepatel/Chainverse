ef66d51c3ffbde4f900742ffd428d649489b0468
maheepatel <mahendarptl9@gmail.com>
2024-02-08 22:56:20 +0530

    Started with the sidebar

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import { Navbar } from "./components";
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main"></div>
+      <div className="footer"></div>
+    </div>
+  );
+};
+
+export default App;
