804d9367ec6ecc40465cc56aa987a4e465165e0f
maheepatel <mahendarptl9@gmail.com>
2024-02-23 21:22:32 +0530

    Done with cryptocurriency page

--- src/Services/cryptoApi.js
+++ src/Services/cryptoApi.js
@@ -1,6 +1,30 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const cryptoApiHeaders = {
+  "X-RapidAPI-Key": "ee0fd1c787msh414cf1a58a2c538p10d5e6jsna13e70e0a9e5",
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+};
 
+const baseUrl = "https://coinranking1.p.rapidapi.com";
 
+const createRequest = (url) => ({
+  url,
+  headers: cryptoApiHeaders,
+});
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+  }),
+});
+
+// redux toolkit creates a hook on calling return all the data for our query.
+// It makes simplerto retrieve data
+// gives us Loading, finished state while making API call
+export const { useGetCryptosQuery } = cryptoApi;
 
 // const options = {
 //   method: 'GET',
--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import { cryptoApi } from "../Services/cryptoApi";
+// import { setupListeners } from "@reduxjs/toolkit/query/react";
 export default configureStore({
-  reducer: {},
+  reducer: {
+    [cryptoApi.reducerPath]: cryptoApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(cryptoApi.middleware),
 });
+
+// middleware: (getDefaultMiddleware) =>
+//   getDefaultMiddleware().concat(cryptoApi.middleware);
